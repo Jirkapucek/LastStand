2ee078d964c68aaa5bb2d83727fd4913c1312466
Jirkapucek <80112275+Jirkapucek@users.noreply.github.com>
2021-06-02 20:12:22 +0200

    Add files via upload

---
+++ tram_2.m
@@ -0,0 +1,11 @@
+% konstanty, počáteční podmínky, interval, kde hledáme řešení %
+g = 9.81; rho = 650; d = 10; x = [-d/2,d/2]; a = 0.25; a1 = 0; a2 = 0; a3 = 0 ; E = 10^5; Izz = 1; n = 20;
+a4 = g*rho*a^2*d/(E*Izz);
+f = @ (x) -g*rho*a^2*1.^(x); % rozložení působící síly %
+L = E*Izz*diffmat([n n+4],4,x); %diferenciální rovnice%
+T1 = diffrow(n+4,0,-d/2,x); T2 = diffrow(n+4,2,d/2,x); T3 = diffrow(n+4,1,-d/2,x); T4 = diffrow(n+4,3,d/2,x); % rovnice příslušné poč. podmínkám%
+% sestavení rovnice a její vyřešení %
+A = [L; T1; T2; T3; T4];
+rhs = [gridsample(f, n); a1; a2; a3; a4];
+u = A\rhs;
+plot(chebfun (u) , '.-'); % vykreslení %
